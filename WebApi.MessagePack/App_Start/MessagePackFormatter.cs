ff677d836124506f6e64f86a5af9c94fc79298c0
liyang <li13526285513@163.com>
2017-12-04 14:02:14 +0800

    添加项目文件。

---
+++ WebApi.MessagePack/App_Start/MessagePackFormatter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using MsgPack;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+using MsgPack.Serialization;
+
+namespace WebApi.MessagePack.App_Start
+{
+    public class MessagePackFormatter : MediaTypeFormatter
+    {
+        Func<Type, bool> IsAllowedType = (t) =>
+        {
+            if (!t.IsAbstract && !t.IsInterface && t != null && !t.IsNotPublic)
+                return true;
+            if (typeof(IEnumerable).IsAssignableFrom(t))
+                return true;
+            return false;
+        };
+        public MessagePackFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/x-msgpack"));
+        }
+        public override bool CanWriteType(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("Type is null");
+            return IsAllowedType(type);
+        }
+        public override Task WriteToStreamAsync(Type type, object value, System.IO.Stream stream, HttpContent content, TransportContext transportContext)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type is null");
+            if (stream == null)
+                throw new ArgumentNullException("Write stream is null");
+            var tcs = new TaskCompletionSource<object>();
+            if (type != typeof(string) && typeof(IEnumerable).IsAssignableFrom(type))
+            {
+                value = (value as IEnumerable<object>).ToList();
+            }
+            var serializer = MessagePackSerializer.Get<dynamic>();
+            serializer.Pack(stream, value);
+            tcs.SetResult(null);
+            return tcs.Task;
+        }
+        public override Task<object> ReadFromStreamAsync(Type type, Stream stream, HttpContent content, IFormatterLogger formatterLogger)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            if (content.Headers != null && content.Headers.ContentLength == 0)
+                return null;
+            try
+            {
+                var serializer = MessagePackSerializer.Get(type);
+                object result;
+                using (var mpUnpacker = Unpacker.Create(stream))
+                {
+                    mpUnpacker.Read();
+                    result = serializer.UnpackFrom(mpUnpacker);
+                }
+                tcs.SetResult(result);
+            }
+            catch (Exception e)
+            {
+                if (formatterLogger == null) throw;
+                formatterLogger.LogError(String.Empty, e.Message);
+                tcs.SetResult(GetDefaultValueForType(type));
+            }
+            return tcs.Task;
+        }
+        public override bool CanReadType(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type is null");
+            return IsAllowedType(type);
+        }
+    }
+}
---
+++ WebApi.MessagePack/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.MessagePack.App_Start;
+
+namespace WebApi.MessagePack
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Filters.Add(new WebApiTrackerAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Add(new ProtoBufFormatter());
+            config.Formatters.Add(new MessagePackFormatter());
+
+            config.Formatters.Add(new ProductCsvFormatter());
+            var bson = new BsonMediaTypeFormatter();
+            bson.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.contoso"));
+            config.Formatters.Add(bson);
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
